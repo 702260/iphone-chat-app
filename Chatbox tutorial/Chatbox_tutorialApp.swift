281bc041a9b55e88984ddf349069c00244771843
Priyanka Soni <priyankasoni@Priyankas-MacBook-Air.local>
2023-07-04 17:11:26 +0530

    initial project files addition

---
+++ Chatbox tutorial/Chatbox_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chatbox_tutorialApp.swift
+//  Chatbox tutorial
+//
+//  Created by Priyanka Soni on 03/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Chatbox_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
